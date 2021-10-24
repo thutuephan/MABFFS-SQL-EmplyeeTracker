7379583db9f591441f32c8149cba95074710b4c0
Thu Phan <alicebe.phan@gmail.com>
2021-10-24 15:24:13 -0500

    Created employee table

--- db/schema.sql
+++ db/schema.sql
@@ -17,7 +17,19 @@ CREATE TABLE role(
     salary DECIMAL(15, 2) NOT NULL,
     department_id INT,
     PRIMARY KEY (id)
-    FOREIGN KEY (department_id) REFERENCE department(id)
-    ON DELETE SET CASCADE
+    FOREIGN KEY (department_id) REFERENCES department(id)
+    ON DELETE CASCADE
 
 );
+
+--Employee table--
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+    ON DELETE CASCADE
+)
