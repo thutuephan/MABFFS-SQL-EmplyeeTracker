55604f87a431e42694ac70047f0a827f6b81ff18
Thu Phan <alicebe.phan@gmail.com>
2021-10-27 02:51:54 -0500

    Removed some extra semi-colons but the errors still appear :(

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,20 +10,20 @@ INSERT INTO role (title, salary, department_id)
 VALUES
     ('CEO', 250000, 1),
     ('Account Manager', 165000, 2),
-    ('Data Analyst', 150000, 4),
-    ('Software Developer', 80000, 4)
-    ('CFO', 210000, 3),
-    ('Sales Manager', 75000, 6),
-    ('HR Director', 130000, 5);
+    ('Data Analyst', 150000, 3),
+    ('Software Developer', 80000, 3)
+    ('CFO', 210000, 2),
+    ('Sales Manager', 75000, 5),
+    ('HR Director', 130000, 4);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
-    ('Mike', 'Stitt', 5, 4),
-    ('Tyler', 'Kuhn', 1, 3);
-    ('Jessica', 'Dunnewold', 2, 1);
-    ('Lisa', 'Russell', 3, null);
-    ('Retihaj', 'Bardwaj', 4, 5);
-    ('Becca', 'Sun', 2, 6);
-    ('Simmon', 'Wade', 4, null);
-    ('Neal', 'Arnold', 3, 4);
-    ('Charlotte', 'Coleman', 5, 1);
+    ('Mike', 'Stitt', 1, 4),
+    ('Tyler', 'Kuhn', 2, 3),
+    ('Jessica', 'Dunnewold', 3, 1),
+    ('Lisa', 'Russell', 4, null),
+    ('Retihaj', 'Bardwaj', 5, 5),
+    ('Becca', 'Sun', 5, 4),
+    ('Simmon', 'Wade', 3, null),
+    ('Neal', 'Arnold', 4, 4),
+    ('Charlotte', 'Coleman', 2, 11);
